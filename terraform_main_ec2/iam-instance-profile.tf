6838ceff129051e9fd4c25688f7592bed9c07318
raji2021max <raji.veera2003@gmail.com>
2025-04-29 22:28:27 +0530

    Update iam-instance-profile.tf

--- terraform_main_ec2/iam-instance-profile.tf
+++ terraform_main_ec2/iam-instance-profile.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_instance_profile" "eks_profile" {
-  name = "veera-eks-worker-profile-unique123"
-  role = aws_iam_role.eks_role.name
+rresource "aws_iam_instance_profile" "instance-profile" {
+  name = "naresh-veera-profile"
+  role = aws_iam_role.iam-role.name
 }
